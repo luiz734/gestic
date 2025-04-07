7021bf8fa42a2387d0c87a7a28a504cb5466ddd7
Luiz G. <luiz007@live.com>
2025-04-07 20:24:26 -0300

    Implement snapshots slice

--- main.go
+++ main.go
@@ -2,23 +2,25 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"time"
-
-	"github.com/dustin/go-humanize"
 )
 
 type Snapshot struct {
-	id   string
-	date time.Time
-	size uint64
+	id      string
+	date    time.Time
+	size    uint64
+	sizeStr string
+	path    string
 }
 
 func (s Snapshot) String() string {
-	return fmt.Sprintf("%s\t%s\t%s\n", s.id, (s.date.Format("2006-02-01 15:04:05")), humanize.Bytes(s.size))
+	return fmt.Sprintf("%s\t%s\t%s\n", s.id, (s.date.Format("2006-01-02 15:04:05")), s.sizeStr)
 }
 
 func parseCmdSnapshots(rawOutput []byte) []Snapshot {
@@ -28,25 +30,88 @@ func parseCmdSnapshots(rawOutput []byte) []Snapshot {
 	tokens := strings.Split(string(rawOutput), "\n")
 	for _, t := range tokens[2 : len(tokens)-3] {
 		lineData := strings.Fields(t)
-		layout := "2006-01-02 15:04:05"
-		timeStr := fmt.Sprintf("%s %s",
+		// TODO: remove hardcoded timezone
+		layout := "2006-01-02 15:04:05-07:00"
+		timeStr := fmt.Sprintf("%s %s%s",
 			strings.TrimSpace(lineData[1]),
 			strings.TrimSpace(lineData[2]),
+			"-03:00",
 		)
 		t, err := time.Parse(layout, timeStr)
 		if err != nil {
 			panic(err)
 		}
+
+		sizeStr := fmt.Sprintf("%s%s",
+			strings.TrimSpace(lineData[len(lineData)-2]),
+			strings.TrimSpace(lineData[len(lineData)-1]),
+		)
+
 		s := Snapshot{
-			id:   strings.TrimSpace(lineData[0]),
-			date: t,
-			size: uint64(123),
+			id:      strings.TrimSpace(lineData[0]),
+			date:    t,
+			size:    uint64(123),
+			sizeStr: sizeStr,
 		}
 		snapshots = append(snapshots, s)
-		fmt.Printf("%s", s)
 	}
 
 	return snapshots
+}
+
+func snapshotContainsTime(s []Snapshot, t time.Time) (int, error) {
+	for index, x := range s {
+		fmt.Printf("%s\n", x.date.String())
+		if x.date.Compare(t) == 0 {
+			return index, nil
+		}
+	}
+	err := errors.New("snapshot does not contain time")
+	return -1, err
+}
+func dirSize(path string) (uint64, error) {
+	var size int64
+	err := filepath.Walk(path, func(dir string, info os.FileInfo, err error) error {
+		fmt.Printf("%s\n", dir)
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			size += info.Size()
+		}
+		return err
+	})
+	return uint64(size), err
+}
+
+func fillSizeAndPath(s []Snapshot) []Snapshot {
+	rootDir := "/home/tohru/tmp/restic/snapshots"
+	entries, err := os.ReadDir(rootDir)
+	if err != nil {
+		errMsg := fmt.Sprintf("Directory not mounted: %s", err)
+		panic(errMsg)
+	}
+	for _, entry := range entries {
+		layout := "2006-01-02T15:04:05-07:00"
+		t, err := time.Parse(layout, entry.Name())
+		if entry.Name() == "latest" {
+			continue
+		}
+		if err != nil {
+			panic(err)
+		}
+		_, err = snapshotContainsTime(s, t)
+		if err != nil {
+			panic("Missing matching time: " + err.Error())
+		}
+		//fullPath := path.Join(rootDir, entry.Name())
+		//dSize, err := dirSize(fullPath)
+		//if err != nil {
+		//	s[tIndex].size = dSize
+		//}
+	}
+
+	return s
 }
 
 func main() {
@@ -78,6 +143,10 @@ func main() {
 	if err != nil {
 		panic(fmt.Sprintf("Error reading output", cmd))
 	}
-	parseCmdSnapshots(output)
+	snapshots := parseCmdSnapshots(output)
+	snapshots = fillSizeAndPath(snapshots)
 	// fmt.Printf("%s", output)
+	for _, s := range snapshots {
+		fmt.Printf("%s", s)
+	}
 }
