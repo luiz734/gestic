dc26d85a2de0147c93069f858f0cfc8f54c3e17d
Luiz G. <luiz007@live.com>
2025-04-07 20:58:06 -0300

    Clean Up

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"bytes"
-	"errors"
 	"fmt"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"strings"
 	"time"
 )
@@ -20,35 +18,44 @@ type Snapshot struct {
 }
 
 func (s Snapshot) String() string {
-	return fmt.Sprintf("%s\t%s\t%s\n", s.id, (s.date.Format("2006-01-02 15:04:05")), s.sizeStr)
+	layout := "2006-01-02 15:04:05"
+	return fmt.Sprintf("%s\t%s\t%s\n", s.id, s.date.Format(layout), s.sizeStr)
 }
 
-func parseCmdSnapshots(rawOutput []byte) []Snapshot {
+func parseCmdSnapshots(rawOutput []byte) ([]Snapshot, error) {
 	var snapshots []Snapshot
 
 	// Split and remove header/footer
 	tokens := strings.Split(string(rawOutput), "\n")
-	for _, t := range tokens[2 : len(tokens)-3] {
-		lineData := strings.Fields(t)
+	start := 2
+	end := len(tokens) - 3
+
+	if end <= start {
+		errMsg := fmt.Errorf("expected at least 1 snapshot")
+		return []Snapshot{}, errMsg
+	}
+
+	for _, t := range tokens[start:end] {
+		fields := strings.Fields(t)
 		// TODO: remove hardcoded timezone
 		layout := "2006-01-02 15:04:05-07:00"
 		timeStr := fmt.Sprintf("%s %s%s",
-			strings.TrimSpace(lineData[1]),
-			strings.TrimSpace(lineData[2]),
+			fields[1],
+			fields[2],
 			"-03:00",
 		)
 		t, err := time.Parse(layout, timeStr)
 		if err != nil {
 			panic(err)
 		}
-
+		// Use the format: X.YYY Gib
 		sizeStr := fmt.Sprintf("%s%s",
-			strings.TrimSpace(lineData[len(lineData)-2]),
-			strings.TrimSpace(lineData[len(lineData)-1]),
+			fields[len(fields)-2],
+			fields[len(fields)-1],
 		)
 
 		s := Snapshot{
-			id:      strings.TrimSpace(lineData[0]),
+			id:      fields[0],
 			date:    t,
 			size:    uint64(123),
 			sizeStr: sizeStr,
@@ -56,96 +63,78 @@ func parseCmdSnapshots(rawOutput []byte) []Snapshot {
 		snapshots = append(snapshots, s)
 	}
 
-	return snapshots
+	return snapshots, nil
 }
 
-func snapshotContainsTime(s []Snapshot, t time.Time) (int, error) {
-	for index, x := range s {
-		fmt.Printf("%s\n", x.date.String())
+func snapshotContainsTime(s []Snapshot, t time.Time) bool {
+	for _, x := range s {
 		if x.date.Compare(t) == 0 {
-			return index, nil
+			return true
 		}
 	}
-	err := errors.New("snapshot does not contain time")
-	return -1, err
-}
-func dirSize(path string) (uint64, error) {
-	var size int64
-	err := filepath.Walk(path, func(dir string, info os.FileInfo, err error) error {
-		fmt.Printf("%s\n", dir)
-		if err != nil {
-			return err
-		}
-		if !info.IsDir() {
-			size += info.Size()
-		}
-		return err
-	})
-	return uint64(size), err
+	return false
 }
 
-func fillSizeAndPath(s []Snapshot) []Snapshot {
+// Checks if the output of `restic snapshots` has a directory
+// associated with each entry. It compares the time for the
+// command output with the filename in the snapshots directory
+func checkDirectoriesConsistency(s []Snapshot) ([]Snapshot, error) {
 	rootDir := "/home/tohru/tmp/restic/snapshots"
-	entries, err := os.ReadDir(rootDir)
+	dateTimeLayout := "2006-01-02T15:04:05-07:00"
+
+	dirEntries, err := os.ReadDir(rootDir)
 	if err != nil {
-		errMsg := fmt.Sprintf("Directory not mounted: %s", err)
-		panic(errMsg)
+		errMsg := fmt.Errorf("directory missing or not mounted: %w", err)
+		return []Snapshot{}, errMsg
 	}
-	for _, entry := range entries {
-		layout := "2006-01-02T15:04:05-07:00"
-		t, err := time.Parse(layout, entry.Name())
+	for _, entry := range dirEntries {
+		t, err := time.Parse(dateTimeLayout, entry.Name())
+		// The directory has a symlink to the most recent snapshot. We ignore it
 		if entry.Name() == "latest" {
 			continue
 		}
+		// Bad naming and it is not the previous case. It should never happen.
 		if err != nil {
 			panic(err)
 		}
-		_, err = snapshotContainsTime(s, t)
-		if err != nil {
-			panic("Missing matching time: " + err.Error())
+		if !snapshotContainsTime(s, t) {
+			errMsg := fmt.Errorf("mismatch entries for snapshot %s", entry.Name())
+			panic(errMsg)
 		}
-		//fullPath := path.Join(rootDir, entry.Name())
-		//dSize, err := dirSize(fullPath)
-		//if err != nil {
-		//	s[tIndex].size = dSize
-		//}
 	}
 
-	return s
+	return s, nil
 }
 
 func main() {
 	var err error
-	_ = err
 	args := []string{"-r", "/mnt/storage/__restic", "snapshots"}
 	var cmd *exec.Cmd
 	if cmd = exec.Command("restic", args...); cmd == nil {
 		panic(fmt.Sprintf("Error running restic command: %v", cmd))
 	}
-	// cb7483d865dc145c31176c6bfecc299d1381b82a40adb0ed4cb9ff5cd53f5269
+	// Temporary key id: cb7483d865dc145c31176c6bfecc299d1381b82a40adb0ed4cb9ff5cd53f5269
 	key := "123"
 	var stdin bytes.Buffer
 	stdin.Write([]byte(key))
 	cmd.Stdin = &stdin
 	// cmd.Stdin = os.Stdin
-
 	// cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	s := Snapshot{
-		id:   "1234",
-		date: time.Now(),
-		size: 1231412,
-	}
-	_ = s
-
 	output, err := cmd.Output()
 	if err != nil {
-		panic(fmt.Sprintf("Error reading output", cmd))
+		errMsg := fmt.Sprintf("Error return from restic command: %v", err)
+		panic(errMsg)
 	}
-	snapshots := parseCmdSnapshots(output)
-	snapshots = fillSizeAndPath(snapshots)
-	// fmt.Printf("%s", output)
+	snapshots, err := parseCmdSnapshots(output)
+	if err != nil {
+		panic(err)
+	}
+	snapshots, err = checkDirectoriesConsistency(snapshots)
+	if err != nil {
+		panic(err)
+	}
 	for _, s := range snapshots {
 		fmt.Printf("%s", s)
 	}
