29d1dc78a22eea41071ecdd6dc7d9772e0cc32a8
Luiz G. <luiz007@live.com>
2025-04-11 05:21:59 -0300

    Add bubble "table" selector model
    Replaced manual table for consistency.

--- models/selector/model.go
+++ models/selector/model.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"gestic/models/compare"
 	"gestic/restic"
+	"github.com/charmbracelet/bubbles/table"
 	"strings"
 
 	"github.com/charmbracelet/bubbletea"
@@ -15,8 +16,7 @@ type Model struct {
 	snapshots   []restic.Snapshot
 	snapshotNew int
 	snapshotOld int
-	cursor      int
-	debug       string
+	table       table.Model
 }
 
 type SnapshotSelectionMsg struct {
@@ -48,13 +48,46 @@ func (m Model) advanceToCompare() (tea.Model, tea.Cmd) {
 	)
 }
 
+func (m Model) UpdateRows() []table.Row {
+	var t []table.Row
+
+	for index, s := range m.snapshots {
+		checked := " "
+		switch index {
+		case m.snapshotNew:
+			checked = "1"
+		case m.snapshotOld:
+			checked = "2"
+		}
+		t = append(t, []string{
+			checked,
+			s.Id,
+			s.Date.Format("2006-01-02 15:04:05"),
+			s.SizeStr,
+		})
+	}
+
+	return t
+}
 func InitialModel(s []restic.Snapshot) Model {
+	columns := []table.Column{
+		{Title: " ", Width: 1},
+		{Title: "Id", Width: 10},
+		{Title: "Date", Width: 20},
+		{Title: "Size", Width: 10},
+	}
 	m := Model{
 		snapshots:   s,
 		snapshotNew: -1,
 		snapshotOld: -1,
-		cursor:      len(s) - 1,
+		table: table.New(
+			table.WithColumns(columns),
+			table.WithFocused(true),
+			table.WithHeight(10),
+		),
 	}
+	m.table.SetRows(m.UpdateRows())
+	m.table.GotoBottom()
 	return m
 }
 
@@ -73,59 +106,36 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "ctrl+c", "q":
 			return m, tea.Quit
-		case "j":
-			m.cursor += 1
-			if m.cursor > len(m.snapshots)-1 {
-				m.cursor -= 1
-			}
-			return m, nil
-		case "k":
-			m.cursor -= 1
-			if m.cursor < 0 {
-				m.cursor += 1
-			}
-			return m, nil
 		case " ":
 			if m.snapshotNew == -1 {
-				m.snapshotNew = m.cursor
-			} else if m.snapshotNew != m.cursor {
-				m.snapshotOld = m.cursor
+				m.snapshotNew = m.table.Cursor()
+			} else if m.snapshotNew != m.table.Cursor() {
+				m.snapshotOld = m.table.Cursor()
 			}
+			m.table.SetRows(m.UpdateRows())
 			return m, nil
 		case "backspace":
 			m.snapshotNew = -1
 			m.snapshotOld = -1
+			m.table.SetRows(m.UpdateRows())
 			return m, nil
 		case "enter":
 			if m.snapshotNew == -1 || m.snapshotOld == -1 {
 				return m, nil
 			}
 			return m.advanceToCompare()
-		default:
-			m.debug = fmt.Sprintf("%#v", msg.String())
-			return m, nil
 		}
 	}
-	return m, nil
+	var cmd tea.Cmd
+	m.table, cmd = m.table.Update(msg)
+	return m, cmd
 }
 
 func (m Model) View() string {
 	var output strings.Builder
 
-	for index, s := range m.snapshots {
-		var lineEnd = ""
-		if m.snapshotNew == index {
-			lineEnd = " [1]"
-		} else if m.snapshotOld == index {
-			lineEnd = " [2]"
-		}
-
-		if index == m.cursor {
-			output.WriteString(fmt.Sprintf(">%s%s\n", s, lineEnd))
-		} else {
-			output.WriteString(fmt.Sprintf(" %s%s\n", s, lineEnd))
-		}
-	}
+	output.WriteString(m.table.View())
+	output.WriteString("\n")
 
 	var footer string
 	if m.snapshotNew != -1 {
@@ -135,8 +145,6 @@ func (m Model) View() string {
 		footer += fmt.Sprintf("\n%s %s", "[2]", m.snapshots[m.snapshotOld].Path)
 	}
 	output.WriteString(footer)
-
-	output.WriteString(fmt.Sprintf("\n\nDEBUG: %s", m.debug))
 
 	return output.String()
 }
