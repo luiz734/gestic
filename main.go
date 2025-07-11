ce39dc308d0ee7560b5d123ef8d5af076ab19a56
Luiz G. <luiz007@live.com>
2025-07-11 09:35:05 -0300

    Add debug support using environment DEBUG=1

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"gestic/config"
 	"gestic/models/selector"
 	"gestic/restic"
+	"log"
 	"os"
 
 	"github.com/alecthomas/kong"
@@ -12,6 +13,16 @@ import (
 )
 
 func main() {
+
+	if len(os.Getenv("DEBUG")) > 0 {
+		f, err := tea.LogToFile("debug.log", "debug")
+		if err != nil {
+			fmt.Println("fatal:", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+	}
+	log.Println("Application started")
 
 	var cli config.CLI
 	_ = kong.Parse(&cli)
