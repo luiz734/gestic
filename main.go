1a5ce5292fdf2d7c3b1ada662bc36044b0664d54
Luiz G. <luiz007@live.com>
2026-04-12 11:34:54 -0300

    Fix DEBUG environment usage
    When DEBUG=0 send logs to /dev/null.

--- main.go
+++ main.go
@@ -18,15 +18,6 @@ var (
 
 func main() {
 
-	if len(os.Getenv("DEBUG")) > 0 {
-		f, err := tea.LogToFile("debug.log", "debug")
-		if err != nil {
-			fmt.Println("fatal:", err)
-			os.Exit(1)
-		}
-		defer f.Close()
-	}
-
 	var cli config.CLI
 	ctx := kong.Parse(&cli,
 		kong.Name("gestic"),
@@ -36,23 +27,39 @@ func main() {
 		},
 	)
 
+	// It returns error if no subcommand was provided
+	// We don't use subcommands and are ignoring errors
 	err := ctx.Run()
-	// We don't use subcommands and are ignoring errors.
-	// if err != nil {
-	// 	ctx.FatalIfErrorf(err)
-	// }
+	//if err != nil {
+	//	ctx.FatalIfErrorf(err)
+	//}
 
 	snapshots, err := restic.GetSnapshots(cli.RepoPath, cli.MountPath)
 	if err != nil {
-		panic(err)
+		_, _ = fmt.Fprintf(os.Stderr, "Error: cannot get snapshots: %v\n\n", err)
+		_, _ = fmt.Fprintf(os.Stderr, "Did you mount the repository?\n")
+		_, _ = fmt.Fprintf(os.Stderr, "Run 'man restic mount' for more information.")
+		os.Exit(1)
 	}
 
 	p := tea.NewProgram(
 		selector.InitialModel(snapshots),
 	)
 
+	// Redirects the debug to a local file
+	debugFile := "/dev/null"
+	if len(os.Getenv("DEBUG")) > 0 {
+		debugFile = "debug.log"
+	}
+	f, err := tea.LogToFile(debugFile, "debug")
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Error: cannot setup debug file:: %v\n", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
 	if _, err := p.Run(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
+		_, _ = fmt.Fprintf(os.Stderr, "Error: program failed to run:: %v\n", err)
 		os.Exit(1)
 	}
 }
